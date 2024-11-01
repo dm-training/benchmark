5eeac10d4385d275a1111140da64d74904523ae9
Малинин Дмитрий Юрьевич <diu.malinin@severstal.com>
2024-11-01 10:42:36 +0300

    add bench Stream#collect

--- module5-JMH-practice/src/main/java/com/luxoft/trainings/jmh/practice/StringConcat.java
+++ module5-JMH-practice/src/main/java/com/luxoft/trainings/jmh/practice/StringConcat.java
@@ -10,25 +10,28 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
 import java.io.IOException;
 import java.util.StringJoiner;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
- * Сравнение объединения строк "+", StringBuilder, StringBuffer и StringJoiner
+ * Сравнение объединения строк "+", StringBuilder, StringBuffer, StringJoiner и Stream#collect
  */
 @State(Scope.Benchmark)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 @BenchmarkMode(Mode.AverageTime)
-@Warmup(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
-@Measurement(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
 public class StringConcat {
     private static final int STR_ARRAY_SIZE = 1000;
     private static final String STR = "string-concat|";
 
     private String[] strArr = new String[STR_ARRAY_SIZE];
 
-    @Setup(Level.Invocation)
+    @Setup
     public void setup() {
         //System.out.println("fill str array");
         for (int i = 0; i < STR_ARRAY_SIZE; ++i) {
@@ -37,43 +40,51 @@ public class StringConcat {
     }
 
     @Benchmark
-    public String stringConcat() {
+    public void stringConcat(Blackhole bh) {
         String result = "";
         for (int i = 0; i < STR_ARRAY_SIZE; i++) {
             result += strArr[i];
         }
         //System.out.println("stringConcat: " + result);
-        return result;
+        bh.consume(result);
     }
 
     @Benchmark
-    public String stringBuilder() {
+    public void stringBuilder(Blackhole bh) {
         StringBuilder result = new StringBuilder(STR_ARRAY_SIZE * STR.length());
         for (int i = 0; i < STR_ARRAY_SIZE; i++) {
             result.append(strArr[i]);
         }
         //System.out.println("stringBuilder: " + result);
-        return result.toString();
+        bh.consume(result.toString());
     }
 
     @Benchmark
-    public String stringBuffer() {
+    public void stringBuffer(Blackhole bh) {
         StringBuffer result = new StringBuffer(STR_ARRAY_SIZE * STR.length());
         for (int i = 0; i < STR_ARRAY_SIZE; i++) {
             result.append(strArr[i]);
         }
         //System.out.println("stringBuffer: " + result);
-        return result.toString();
+        bh.consume(result.toString());
     }
 
     @Benchmark
-    public String stringJoiner() {
+    public void stringJoiner(Blackhole bh) {
         StringJoiner result = new StringJoiner("");
         for (int i = 0; i < STR_ARRAY_SIZE; i++) {
             result.add(strArr[i]);
         }
         //System.out.println("stringJoiner: " + result);
-        return result.toString();
+        bh.consume(result.toString());
+    }
+
+    @Benchmark
+    public void streamCollect(Blackhole bh) {
+        String result = Stream.of(strArr)
+                .collect(Collectors.joining());
+        //System.out.println("streamCollect: " + result);
+        bh.consume(result);
     }
 
     public static void main(String[] args) throws IOException {
